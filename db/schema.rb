6d9d89a0074211075335a6de12f5ea0f6492a62a
glitchkitten <ahaschenburger@gmail.com>
2016-01-12 13:42:50 +0000

    heroku postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151029155417) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string   "description"
     t.string   "priority"
